893604573c3abb49f6e5f85564eb4e863db5a3b6
sertanrdn <sertanerdogan07@gmail.com>
2025-07-28 12:36:01 +0200

    Created length label, increment-decrement buttons

---
+++ src/components/BreakControl.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './BreakControl.css';
+
+const BreakControl = ({ breakLength, onIncrement, onDecrement }) => {
+    return (
+        <div>
+            <h2 id="break-label">Break Length</h2>
+            <button id="break-decrement" onClick={onDecrement}>-</button>
+            <span id="break-length">{breakLength}</span>
+            <button id="break-increment" onClick={onIncrement}>+</button>
+        </div>
+    );
+}
+
+export default BreakControl;
